2744301c40ff854e9e0ae65fe5439df10b7b11ee
Jacobs Farm <jacobsfarmextras@gmail.com>
2025-11-14 21:58:12 +0100

    updated Start_enviroment.bat
    Now simply add or delete REM to activate a step or disable the step. Now you can either start a enviroment or a script

--- start_enviroment.bat
+++ start_enviroment.bat
@@ -3,16 +3,17 @@ title your_camera_name
 REM Configuration - modify these variables as needed
 REM To see version conda in anaconda prompt = echo %CONDA_PREFIX%
 set CONDA_PATH="C:\ProgramData\anaconda3\Scripts\activate.bat" 
-set PROJECT_DRIVE=D:
+set PROJECT_DRIVE=C:
 set ENVIRONMENT="your_enviroment_name"
 set PROJECT_FOLDER="folder_name"
-REM set SCRIPT_NAME=your_script.py
+set SCRIPT_NAME=your_script.py
 
-REM Navigate to project directory and activate environment
+REM Navigate to the project directory and activate the environment. Add or remove REM to disable or enable a step
 call %CONDA_PATH%
 %PROJECT_DRIVE%
 cd %PROJECT_FOLDER%
-call conda activate %ENVIRONMENT%
+REM call conda activate %ENVIRONMENT%
+REM python %SCRIPT_NAME%
 
 REM python %SCRIPT_NAME%
 REM Show current location and open interactive command prompt
